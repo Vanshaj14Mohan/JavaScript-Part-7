64788ccbb8fe5f93374a5dbbafa80479e592c1e3
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-02 23:59:15 +0530

    Array Methods

--- Array Methods/Every and Sum method/app.js
+++ Array Methods/Every and Sum method/app.js
@@ -7,3 +7,14 @@ let arr = [1,2,3,4,5]
 check = arr.every((el) =>{
     return el % 2 == 0; //would return false as the array contains some negative numbers too.
 });
+
+//Some Method -> Quite similar to logical OR Operator.
+//Returns true if some element of array gives true for some function. Else returns false.
+//arr.sum(some function definition or name)
+
+let data = [1,2,3,4,5]
+
+ data.some((el) =>{
+    return el % 2 == 0; //would return true as the array contains some negative numbers too.
+});
+
