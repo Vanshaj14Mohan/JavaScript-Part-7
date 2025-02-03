0fad15333b1baacb87c22ce3243fe0b7403fe24d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-03 23:29:31 +0530

    Array methods

---
+++ Array Methods/Reduce method/app.js
@@ -0,0 +1,3 @@
+//Reduce method reduces the array to a single value. ie -> if array contains very large value so reduces give a single value in the end.
+//Syntax 
+//arr.reduce(reducer function with 2 variables for (accumulator, element));
