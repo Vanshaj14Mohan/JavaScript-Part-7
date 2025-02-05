ded7d1393e29493eddd56d09b552eb7c50430e0f
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-05 23:09:47 +0530

    Default parameters

--- Array Methods/app.js
+++ Array Methods/app.js
@@ -56,3 +56,18 @@ let nums = [1,2,3,4,5,6,7,8,9,10];
 newNum = nums.filter((el)=>{
     return el % 2 == 0; //will give only even number values
 });
+
+//Default parameters -> Used inside a function for giving a default value to the arguments.
+//eg
+// function func(a, b = 2){
+//     //do something
+// }
+//we can put default parameter in the starting or in the ending of the argument it does'nt matter.
+//(a =1, b) -> this syntax and assigning value like this is wrong.
+//sum(1) so b would be undefined.as a is already assigned a vlaue.
+function sum(a=2,b){
+    return a +b;
+}
+
+sum(2,3);
+//sum(1) would print undefined
