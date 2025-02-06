89f9763c5811da4764479354a1149a4e66d40d1b
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-06 23:55:21 +0530

    Concepts

--- Concepts/app.js
+++ Concepts/app.js
@@ -3,3 +3,9 @@
 // function func(...arr){
  //do something
 //}
+
+let arr = [1,2,3,4,5,6,7]
+console.log(...arr) //it will spread the array elements
+
+data = "Codeandsleep"
+console.log(...data) //it will spread the string elements
