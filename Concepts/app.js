65d3187c6b4b7c4cba39e4fa29e01f04e8fd8240
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-07 23:57:34 +0530

    Spread Concept

--- Concepts/app.js
+++ Concepts/app.js
@@ -11,7 +11,6 @@ data = "Codeandsleep"
 console.log(...data) //it will spread the string elements
 
 //We can use spread with array literals too.
-
 let newArr = [1,2,3,4,5]
 dataTwo = [...newArr]
 console.log(dataTwo);
@@ -23,3 +22,12 @@ let odd = [1,3,5,7,9]
 let even = [2,4,6,8,10]
 result = [...odd, ...even]
 console.log(result);
+
+//Now how we can use Spread with Object Literals
+const dataStudent = {
+    email: "Student12345@gmail.com",
+    password: "abcd",
+};
+
+dataCopy = {...dataStudent, id:123};
+console.log(dataCopy);
