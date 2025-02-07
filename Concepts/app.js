0759e1b7c49eb966e4eca3d7a861c2b202f33003
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-07 23:50:30 +0530

    Spread Concept

--- Concepts/app.js
+++ Concepts/app.js
@@ -9,3 +9,17 @@ console.log(...arr) //it will spread the array elements
 
 data = "Codeandsleep"
 console.log(...data) //it will spread the string elements
+
+//We can use spread with array literals too.
+
+let newArr = [1,2,3,4,5]
+dataTwo = [...newArr]
+console.log(dataTwo);
+
+char = [..."hello"]
+console.log(char);
+
+let odd = [1,3,5,7,9]
+let even = [2,4,6,8,10]
+result = [...odd, ...even]
+console.log(result);
