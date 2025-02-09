d0a209cc074aeef27708afcb62881b44b4f469e6
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-09 18:11:56 +0530

    Destructuring Concept

--- Destructuring/app.js
+++ Destructuring/app.js
@@ -6,3 +6,17 @@ let names = ["John", "Adam", "Mark", "Jack", "Damian"];
 let [winner, runnerup, ...others] = names; //storing starting values of array in a variable, ...others here we used rest concept
 //we can combine rest and destructuring concept together 
 console.log(winner, runnerup); //John, Adam
+
+//Now how we can do Destructuring in objects.
+const student = {
+    name: "John",
+    age: 20,
+    class: "UG",
+    subjects: ["English", "Computer", "Maths"],
+    username: "John@1234",
+    password: "abcd@1234"
+};// now let's start picking specific information from the student
+// let username = student.username;
+// let password = student.password; //2nd way using destructuring here
+let {username, password} = student; //1st way using destructuring here
+// let {username: user, password} = student;
