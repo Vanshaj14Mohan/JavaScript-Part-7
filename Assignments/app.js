ef1867b9b428f2cc1b74bbbbc8f43a999ad72812
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-09 23:07:50 +0530

    Assignments

--- Assignments/app.js
+++ Assignments/app.js
@@ -1,4 +1,4 @@
-//Square and sum of the array elements using the arrow function and then fund the average of the array.
+//1. Square and sum of the array elements using the arrow function and then fund the average of the array.
 const numbers = [1, 2, 3, 4, 5];
 // const squareAndSum = numbers.map(num => num ** 2).reduce((a, b)
 
@@ -8,4 +8,11 @@ let double = numbers.map((el)=>{ //for square
 
 let result = numbers.reduce((res,el)=>{ //for sum
     return res + el;
+});
+
+//2. Create a new array using the map function whose each element is equal to the original element plus 5.
+const numbers2 = [1, 2, 3, 4, 5];
+
+let addFive = numbers2.map((el)=>{
+    return el + 5;
 });
